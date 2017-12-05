32ec882b6194bc9e3b13faa13ce6026ba3e1fb4d
Emil Gardström <emil.gardstrom@gmail.com>
2017-12-05 10:29:55 +0100

    Added simpson and fixed plot output

--- roadster/s.m
+++ roadster/s.m
@@ -1,12 +1,13 @@
 %clear;
+h1 = figure;
 load roadster;
-subplot(4,1,1);
+subplot(2,1,1);
 plot(speed_kmph, consumption_Whpkm, 'x'), hold on
 plot(0:0.1:200,consumption(0:0.1:200),'k')
 title('Roadster')
 xlabel('speed [km/h]')
 ylabel('consumption [Wh/km]')
-subplot(4,1,2);
+subplot(2,1,2);
 battery = 55e3; % Wh
 reach = battery./(consumption(0:0.1:200));
 plot(0:0.1:200, reach)
@@ -19,12 +20,13 @@ max_x = (idx-1)/10;
 plot(max_x,max_val,'o','MarkerSize',10);
 str = ['Maximum = (',num2str(max_x), ', ',num2str(max_val),')'];
 text(60,200,str,'HorizontalAlignment','right');
-subplot(4,1,3);
+h2 = figure;
+subplot(2,1,1);
 plot(0:0.1:65,velocity(0:0.1:65,'speed_elsa'))
 xlabel('distance [km]')
 ylabel('speed [km/h]')
 title('Elsa')
-subplot(4,1,4)
+subplot(2,1,2)
 plot(0:0.1:65,velocity(0:0.1:65,'speed_anna'))
 title('Anna')
 xlabel('distance [km]')
---
+++ roadster/simpson.m
@@ -0,0 +1,13 @@
+function S = simpson(func, a, b, n)
+% S = simpson(func, a, b, n)
+%
+% This function uses Simpson's rule to approximate the
+% integral of the function func over the interval [a, b].
+if (mod(n,2))
+    error('n must be even');
+end
+
+h = (b-a)/n;
+x = a:h:b; % x = linspace(a,b,n+1);
+fx = func(x);
+S = (h/3)*( 4*sum(fx(2:2:end-1)) + 2*sum(fx(3:2:end-2)) + fx(1) + fx(end) );
